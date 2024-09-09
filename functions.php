1df575645b3f087e5082bd8ea409aaf71730b4b3
Wunst <matthiesbe@gmail.com>
2024-09-09 09:33:28 +0200

    Expand footer
    Removed footer CSS for the time being
    TODO: Do not print header/footer (plugin?)

--- functions.php
+++ functions.php
@@ -3,10 +3,10 @@
 add_action("wp_enqueue_scripts", "azbau_enqueue_styles");
 
 function azbau_enqueue_styles() {
-  wp_enqueue_style(
-    "footer", 
-    get_parent_theme_file_uri("assets/css/footer.css")
-  );
+  #wp_enqueue_style(
+  #  "footer", 
+  #  get_parent_theme_file_uri("assets/css/footer.css")
+  #);
 
   wp_enqueue_style(
     "button", 
